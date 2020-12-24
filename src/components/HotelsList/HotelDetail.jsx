11a2a43df8687bf1ac266811e84d67beb39dd0c9
Amit <amitkumar.developer1@gmail.com>
2020-12-25 02:34:37 +0530

    Changes in hotel details page

--- src/components/HotelsList/HotelDetail.jsx
+++ src/components/HotelsList/HotelDetail.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../../css/HotelDetail.css'
+
 const urlHotelDetail = 'https://developerfunnel.herokuapp.com/hotelsdetails/'
 class HotelDetail extends React.Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
         this.state ={
             hotel_data: ''
         }
@@ -25,30 +26,15 @@ class HotelDetail extends React.Component {
                                 <p><strong>Location: </strong>{this.state.hotel_data.locality}</p>
                                 <p><strong>City: </strong>{this.state.hotel_data.city_name}</p>
                                 <p><strong>Cost: &#8377;</strong>{this.state.hotel_data.cost}</p>
-                            </div>
-                            <hr/>
-                            <form className="p-4">
-                                <h4>Booking Details</h4>
-                                <div className="form-row">
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="inputEmail4">Start Date</label>
-                                        <input type="date" className="form-control" id="inputEmail4"/>
-                                    </div>
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="inputPassword4">End Date</label>
-                                        <input type="date" className="form-control" id="inputPassword4"/>
-                                    </div>
-                                </div>
                                 <div className="form-group row">
-                                    <label className="col-sm-2 col-form-label">Room type: </label>
+                                    <label className="col-sm-2 col-form-label"><strong>Room type: </strong></label>
                                     <div className="col-sm-9">
                                         {this.state.hotel_data.type.map((data, index) => {
-                                            return (<button key={index} className="btn btn-light btn-outline-info m-1" id={data.roomtype}>{data.name}</button>)
+                                            return (<p key={index} className="btn btn-light btn-outline-info m-1" id={data.roomtype}>{data.name}</p>)
                                         })}
                                     </div>
                                 </div>
-                                <button className="btn btn-success btn-lg pl-4 pr-4">Go to Booking</button>
-                            </form>
+                            </div>
                         </div>
                     </div>
                 </div>
